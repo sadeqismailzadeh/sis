840212f3830d8980e4a79e3c951516fa44103056
Sadeq Ismailzadeh <ismailzadeh@iasbs.ac.ir>
2020-07-10 18:12:23 +0430

    remove unused

--- Simulation.py
+++ Simulation.py
@@ -22,37 +22,25 @@ while len(sequence) < num_nodes:
     if nextval >= 2 and  nextval <= num_nodes // 2:
         sequence.append(nextval)
 
-print(sequence)
-
 if sum(sequence)%2 == 1:
     sequence[0] += 1
     
 graph = nx.configuration_model(sequence)
-# count parallel edges and avoid counting A-B as well as B-A
-num_edges = sum(len(graph[node][neigh]) for node in graph for neigh in graph.neighbors(node))
-print("multigraph has {0:d} edges".format(num_edges))
-num_par = sum(len(graph[node][neigh]) for node in graph for neigh in graph.neighbors(node)) // 2
-print("multigraph has {0:d} parallel edges".format(num_par))
-num_loops = nx.number_of_selfloops(graph)
-print("multigraph has {0:d} self-loops".format(num_loops))
 
 graph = nx.Graph(graph)
 loops = nx.selfloop_edges(graph)
 graph.remove_edges_from(loops)
+
 # get largest connected component
-# unfortunately, the iterator over the components is not guaranteed to be sorted by size
 largest_cc = max(nx.connected_components(graph), key=len)
 graph = graph.subgraph(largest_cc).copy()
                      
-print("lcc graph has {0:d} nodes".format(nx.number_of_nodes(graph)))
-print("lcc graph has {0:d} edges".format(nx.number_of_edges(graph)))
+print("graph has {0:d} nodes and {1:d} edges".format(nx.number_of_nodes(graph),
+                                                         nx.number_of_edges(graph)))
 
 degree_list = [graph.degree(n) for n in graph]
-
 degreeCount = collections.Counter(degree_list)
 degree, counts = zip(*degreeCount.items())
-#degree = np.log(degree)
-#counts = np.log(counts)
 plt.plot(degree, np.array(counts)/num_nodes, 'ro')
 
 counts = np.bincount(sequence)
@@ -82,7 +70,7 @@ model.set_initial_status(cfg)
 # Simulation execution
 iterations = model.iteration_bunch(50)
 trends = model.build_trends(iterations)
-#%%
+#%% 
 from bokeh.io import output_notebook, show
 from ndlib.viz.bokeh.DiffusionTrend import DiffusionTrend
 
